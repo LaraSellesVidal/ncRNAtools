95629d8f5f95d608965de66e2bb38e8dee095569
Lara Selles <laraselles@dyn3205-118.wlan.ic.ac.uk>
2020-09-29 15:45:36 +0100

    v 0.99.7 change to HTTP to avoid OpenSSL 1.1.1 bug

--- R/ncRNAtools_variableDefinitions.R
+++ R/ncRNAtools_variableDefinitions.R
@@ -1,6 +1,6 @@
-rnaCentralEbiApiURL <- 'https://www.ebi.ac.uk/ebisearch/ws/rest/rnacentral'
-rnaCentralApiURL <- 'https://rnacentral.org/api/v1/rna'
-rnaCentralRangeSearchURL <- 'https://rnacentral.org/api/v1/overlap/region'
+rnaCentralEbiApiURL <- 'http://www.ebi.ac.uk/ebisearch/ws/rest/rnacentral'
+rnaCentralApiURL <- 'http://rnacentral.org/api/v1/rna'
+rnaCentralRangeSearchURL <- 'http://rnacentral.org/api/v1/overlap/region'
 
 rtoolsBaseURL <- 'http://rtools.cbrc.jp/'
 
